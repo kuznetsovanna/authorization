bd26df7e6a753da4896ea38818ca860a883d566e
kuznetsovanna <rublevanna@gmail.com>
2021-07-22 23:19:08 +0400

    Рефакторинг

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function setErrorEmail(message) {
-    var err = document.getElementById("erreml");
+    const err = document.getElementById("erreml");
     err.innerHTML = message;
 }
 
-function SetErrorPassword(message) {
-    var err = document.getElementById("errpwd");
+function setErrorPassword(message) {
+    const err = document.getElementById("errpwd");
     err.innerHTML = message;
 }
 
@@ -29,80 +29,84 @@ function validate() {
 
     // проверка пароля
     if (textPass.length === 0) {
-        SetErrorPassword('Введите пароль');
+        setErrorPassword('Введите пароль');
         result = false;
     } else if (textPass.length < 6) {
-        SetErrorPassword('Пароль должен быть минимум 6 символов');
+        setErrorPassword('Пароль должен быть минимум 6 символов');
         result = false;
     } else if (textPass.search(/[a-zA-Z]/) === -1) {
-        SetErrorPassword('Пароль должен содержать буквы разных регистров');
+        setErrorPassword('Пароль должен содержать буквы разных регистров');
         result = false;
     } else if (textPass.search(/[0-9]/) === -1) {
-        SetErrorPassword('Пароль должен содержать цифры');
+        setErrorPassword('Пароль должен содержать цифры');
         result = false;
     } else if (textPass.search(/[!@#$%^&*()_+"№;%:?\]]/) === -1) {
-        SetErrorPassword('Пароль должен содержать спец.символы');
+        setErrorPassword('Пароль должен содержать спец.символы');
         result = false;
     } else {
-        SetErrorPassword('');
+        setErrorPassword('');
     }
 
     return result;
 }
+
 // день недели
 function getWeekDay(date) {
     let days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
 
     return days[date.getDay()];
 }
-    // часы
-    function clockTimer() {
-        let date = new Date();
-        let time = [date.getHours(), date.getMinutes(), date.getSeconds()]; // |[0] = Hours| |[1] = Minutes| |[2] = Seconds|
 
-        if (time[0] < 10) {
-            time[0] = "0" + time[0];
-        }
-        if (time[1] < 10) {
-            time[1] = "0" + time[1];
-        }
-        if (time[2] < 10) {
-            time[2] = "0" + time[2];
-        }
+// часы
+function clockTimer() {
+    let date = new Date();
+    let time = [date.getHours(), date.getMinutes(), date.getSeconds()]; // |[0] = Hours| |[1] = Minutes| |[2] = Seconds|
 
-        let current_time = time.join(":");
-
-        // день
-        let dd  = date.getDate().toString();
-        if (dd < 10) {
-            dd = "0" + dd;
-        }
-        // месяц
-        let mm = (date.getMonth() + 1).toString();
-        if (mm < 10) {
-            mm = "0" + mm;
-        }
-        // год
-        let yyyy = date.getFullYear().toString();
-        // день недели
-        let w = getWeekDay(date);
-
-        // дата и день недели
-        let current_date = dd+'.'+mm+'.'+yyyy+' '+w+' ';
-
-        let clock = document.getElementById("clock");
-
-        clock.innerHTML = current_date + current_time;
-
-        setTimeout("clockTimer()", 1000);
+    if (time[0] < 10) {
+        time[0] = "0" + time[0];
+    }
+    if (time[1] < 10) {
+        time[1] = "0" + time[1];
+    }
+    if (time[2] < 10) {
+        time[2] = "0" + time[2];
     }
 
-    setTimeout("clockTimer()", 10);
+    let current_time = time.join(":");
 
-function getNameBrowser(){
+    // день
+    let dd = date.getDate().toString();
+    if (dd < 10) {
+        dd = "0" + dd;
+    }
+    // месяц
+    let mm = (date.getMonth() + 1).toString();
+    if (mm < 10) {
+        mm = "0" + mm;
+    }
+    // год
+    let yyyy = date.getFullYear().toString();
+    // день недели
+    let w = getWeekDay(date);
+
+    // дата и день недели
+    let current_date = dd + '.' + mm + '.' + yyyy + ' ' + w + ' ';
+
+    let clock = document.getElementById("clock");
+
+    clock.innerHTML = current_date + current_time;
+
+    setTimeout("clockTimer()", 1000);
+}
+
+function getNameBrowser() {
     var ua = navigator.userAgent;
 
 
-if (ua.search(/rv:11.0/) > 0){
-        alert('используйте другой браузер');}
+    if (ua.search(/rv:11.0/) > 0) {
+        alert('используйте другой браузер');
     }
+}
+
+
+setTimeout("clockTimer()", 10);
